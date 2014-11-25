e44a1be6cfc26a701583759268d4de9d068164ea
sundari22 <sundari.arulmani@gmail.com>
2014-11-26 03:45:48 +0530

    Update search.js

--- search.js
+++ search.js
@@ -21,7 +21,7 @@ jQuery.ajaxSetup({async:false});
   requestUserUploadsPlaylistId();
  
 //});
-  $('#login-link').hover(function(){
+  //$('#login-link').hover(function(){
   //var logout = function(){
 //document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/";
 //}
