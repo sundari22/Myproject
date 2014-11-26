1249a6f177b841446ea933215a06360054d12ae5
sundari22 <sundari.arulmani@gmail.com>
2014-11-26 16:35:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,70 +44,7 @@ googleApiClientReady = function() {
   function loadAPIClientInterfaces(response) {
   console.log(response);
   gapi.client.load('youtube', 'v3', function() {
-    var q, playlistId, k, nextPageToken, prevPageToken;
-var videoarray = [];
-    
-  // See https://developers.google.com/youtube/v3/docs/channels/list
-    var request = gapi.client.youtube.playlists.list({
-    mine: true,
-    part: 'id, snippet, contentDetails'
-  });
- 
-  request.execute(function(response) {
-    
-    console.log(response);
-    for (i in response.result.items){
-    q=child;   
-    var re =new RegExp(q, "gi")
-
-    playlistId = response.result.items[i].snippet.title;
-    if (playlistId.match(re)) { 
-    playlistId = response.result.items[i].id;
-    requestVideoPlaylist(playlistId);
-   // Retrieve the list of videos in the specified playlist.
-    function requestVideoPlaylist(playlistId, pageToken) {
-   $('#video-container').html('');
-   var requestOptions = {
-    playlistId: playlistId,
-    part: 'snippet',
-    maxResults: 10
-    };
-    if (pageToken) {
-    requestOptions.pageToken = pageToken;
-    }
-    var request = gapi.client.youtube.playlistItems.list(requestOptions);
-    request.execute(function(response) {
-    
-    var playlistItems = response.result.items;
-    if (playlistItems) {
-      
-      $.each(playlistItems, function(index, item) {
-      var entry = {};
-      entry.video_id = item.snippet.resourceId.videoId;
-      entry.playlistId = item.snippet.playlistId;
-      entry.image_src = item.snippet.thumbnails.medium.url;
-      
-      videoarray.push(entry); 
-      });       
-      } else {
-      alert('Hello');
-      $('.content2').html('Sorry you have no uploaded videos');
-      $('.content2').append('<iframe width="460" height="315" src="//www.youtube.com/embed/4D5rvmw1qUU" frameborder="0" align=center allowfullscreen></iframe>');
-    }
+    handleAPILoaded();
   });
 }
-}
-}
-
-if (videoarray.length != 0 ){
-
-$('.content2').append('<iframe width="460" height="315" src="//www.youtube.com/embed/videoarray[1].video_id?playlist='+ videoarray[0].video_id+', '+videoarray[1].video_id +','+ videoarray[2].video_id +'&showinfo=1" frameborder="0" align="center" allowfullscreen></iframe>');
-}
-
-});
-}
-
-    
-    
-  });
-}
+   
