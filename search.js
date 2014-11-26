8aa7b068502f76f1bc538aa4b77093f4860f868a
sundari22 <sundari.arulmani@gmail.com>
2014-11-26 20:19:04 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,10 +1,15 @@
 
-// After the API loads, call a function to enable the search box.
-function handleAPILoaded() {
-	x=child;
-	console.log(x)
-	gapi.client.load('youtube', 'v3').then(function() { console.log('loaded.'); });
-  $('#search-button').attr('disabled', false);
+googleApiClientReady = function() {
+gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+ }
+
+ // Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() { 
+   // This API key is intended for use only in this lesson. 
+   // See http://goo.gl/PdPA1 to get a key for your own applications.
+   gapi.client.setApiKey('AIzaSyCUop_qKqfZHRTy09LGfpSt8cNcHsrmvIY'); 
+
+$('#search-button').attr('disabled', false);
 }
 
 var q, playlistId, k, nextPageToken, prevPageToken;
@@ -20,59 +25,35 @@ $(document).on('pageinit',function() {
 		
   
   q = $('#search').val();
-  console.log(q);  
+    
   requestUserUploadsPlaylistId();
  
 });
-  //$('#login-link').hover(function(){
-  //var logout = function(){
-//document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/";
-//}
-  //window.open('https://mail.google.com/a/YOURDOMAIN.IN/?logout&hl=en','logout_from_google','width=600,height=300,menubar=no,status=no,location=no,toolbar=no,scrollbars=no,top=20,left=20');
-  //gapi.auth.signOut();
-//});
-//});
-}
+});
+  
+
+
+  
 function requestUserUploadsPlaylistId() {
   // See https://developers.google.com/youtube/v3/docs/channels/list
-    var request = gapi.client.youtube.playlists.list({
-    mine: true,
-    part: 'id, snippet, contentDetails'
+    var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet',
+    maxResults:10
   });
- 
+    
   request.execute(function(response) {
     
-    console.log(response);
-    for (i in response.result.items){
-     
-    var re =new RegExp(q, "gi")
-
-    playlistId = response.result.items[i].snippet.title;
-    if (playlistId.match(re)) { 
-    playlistId = response.result.items[i].id;
-    requestVideoPlaylist(playlistId);
-   // Retrieve the list of videos in the specified playlist.
-    function requestVideoPlaylist(playlistId, pageToken) {
-   $('#video-container').html('');
-   var requestOptions = {
-    playlistId: playlistId,
-    part: 'snippet',
-    maxResults: 10
-    };
-    if (pageToken) {
-    requestOptions.pageToken = pageToken;
-    }
-    var request = gapi.client.youtube.playlistItems.list(requestOptions);
-    request.execute(function(response) {
     
+    console.log(response);
     var playlistItems = response.result.items;
     if (playlistItems) {
       
       $.each(playlistItems, function(index, item) {
       var entry = {};
-      entry.video_id = item.snippet.resourceId.videoId;
-      entry.playlistId = item.snippet.playlistId;
-      entry.image_src = item.snippet.thumbnails.medium.url;
+      entry.video_id = item.id.videoId;
+      //entry.playlistId = item.snippet.playlistId;
+      //entry.image_src = item.snippet.thumbnails.medium.url;
       
       videoarray.push(entry); 
       });       
@@ -82,15 +63,13 @@ function requestUserUploadsPlaylistId() {
       $('.content2').append('<iframe width="460" height="315" src="//www.youtube.com/embed/4D5rvmw1qUU" frameborder="0" align=center allowfullscreen></iframe>');
     }
   });
-}
-}
-}
+
 
 if (videoarray.length != 0 ){
 
-$('.content2').append('<iframe width="460" height="315" src="//www.youtube.com/embed/videoarray[1].video_id?playlist='+ videoarray[0].video_id+', '+videoarray[1].video_id +','+ videoarray[2].video_id +'&showinfo=1" frameborder="0" align="center" allowfullscreen></iframe>');
+$('.content2').append('<iframe width="460" height="315" src="http://www.youtube.com/embed/videoarray[1].video_id?playlist='+ videoarray[0].video_id+', '+videoarray[1].video_id +','+ videoarray[2].video_id +'&showinfo=1" frameborder="0" align="center" allowfullscreen></iframe>');
 }
 
-});
+
 }
 
